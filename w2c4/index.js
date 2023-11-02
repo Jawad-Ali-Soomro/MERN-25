41eb7c47c756bdd5ae3b61a0fe81bcb6059cf942
JawadWebTech <jawadal829@gmail.com>
2023-11-02 18:47:31 +0500

    w2c4 added

---
+++ w2c4/index.js
@@ -0,0 +1,16 @@
+const globalVar = "Hello It's Me Jawad"
+function Hello() {
+    console.log(globalVar);
+}
+Hello()
+console.log(Math.round(1.32144534535436213));
+console.log(Math.ceil(1.1));
+console.log(Math.floor(1.1));
+const nameOfFunction = () => {
+    console.log("hello I am a function");
+}
+const functional = () => console.log("hello I am a function");
+nameOfFunction()
+functional()
+
+// retturn should return something else it will be not  returning something!
